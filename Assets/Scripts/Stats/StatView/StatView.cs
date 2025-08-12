278a6bf1bea23f1e10a506f94f2334526212b0c5
adag21044 <adag21044@gmail.com>
2025-08-12 14:50:16 +0300

    Add StatView UI animation and update methods
    Introduced serialized Image fields and animation duration to StatView. Added methods to animate heart, career, and happiness stat UI elements using DOTween. Updated SampleScene to include a new GameObject with StatView attached.

--- Assets/Scripts/Stats/StatView/StatView.cs
+++ Assets/Scripts/Stats/StatView/StatView.cs
@@ -1,6 +1,34 @@
+using UnityEngine.UI;
 using UnityEngine;
+using DG.Tweening;
 
 public class StatView : MonoBehaviour
 {
-    
+    [SerializeField] private Image heartImage;
+    [SerializeField] private Image careerImage;
+    [SerializeField] private Image happinessImage;
+
+    [SerializeField] private float duration = 0.5f;
+
+    [ContextMenu("Update Heart")]
+    private void UpdateHeartValue()
+    {
+        heartImage.DOFillAmount(0.5f, duration)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() => Debug.Log("Heart value updated to: " + 0.5f));
+    }
+
+    private void UpdateCareerValue(float value)
+    {
+        heartImage.DOFillAmount(value, duration)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() => Debug.Log("Career value updated to: " + value));
+    }
+
+    private void UpdateHappinessValue(float value)
+    {
+        heartImage.DOFillAmount(value, duration)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() => Debug.Log("Happiness value updated to: " + value));
+    }
 }
