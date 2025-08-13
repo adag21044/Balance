a9a7aabd57ab22dee085a357ecf0ce901ac78d7a
adag21044 <adag21044@gmail.com>
2025-08-13 15:11:49 +0300

    Add custom inspector and method attribute for debug tools
    Introduced MasearyInspector and MethodAttribute to enable invoking methods with parameters from the Unity Inspector for MonoBehaviours. Updated StatView to use the new [Method] attribute for value update methods. Modified SampleScene to add GameManager objects and link StatView reference.

--- Assets/Scripts/Stats/StatView/StatView.cs
+++ Assets/Scripts/Stats/StatView/StatView.cs
@@ -10,24 +10,26 @@ public class StatView : MonoBehaviour
 
     [SerializeField] private float duration = 0.5f;
 
-    [ContextMenu("Update Heart")]
+    [Method]
     public void UpdateHeartValue(float value)
     {
-        heartImage.DOFillAmount(0.5f, duration)
+        heartImage.DOFillAmount(value, duration)
             .SetEase(Ease.InOutSine)
-            .OnComplete(() => Debug.Log("Heart value updated to: " + 0.5f));
+            .OnComplete(() => Debug.Log("Heart value updated to: " + value));
     }
 
+    [Method]
     public void UpdateCareerValue(float value)
     {
-        heartImage.DOFillAmount(value, duration)
+        careerImage.DOFillAmount(value, duration)
             .SetEase(Ease.InOutSine)
             .OnComplete(() => Debug.Log("Career value updated to: " + value));
     }
 
+    [Method]
     public void UpdateHappinessValue(float value)
     {
-        heartImage.DOFillAmount(value, duration)
+        happinessImage.DOFillAmount(value, duration)
             .SetEase(Ease.InOutSine)
             .OnComplete(() => Debug.Log("Happiness value updated to: " + value));
     }
